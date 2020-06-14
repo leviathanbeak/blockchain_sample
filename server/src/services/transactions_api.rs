081f150f390a6042ef3bf45f2932219af32016f9
leviathanbeak88 <elvisdedic@outlook.com>
2020-06-14 09:15:55 +0200

    implemented tx broadcast

--- server/src/services/transactions_api.rs
+++ server/src/services/transactions_api.rs
@@ -1,33 +1,55 @@
 use crate::AppState;
-use actix_web::{error, web, HttpResponse, Resource};
+use actix_web::{error, web, HttpResponse, Scope};
 use blockchain::transaction::Transaction;
-use bytes::BytesMut;
-use futures::StreamExt;
+use reqwest::Client;
 use serde_json::json;
 
-pub fn init_service() -> Resource {
-    web::resource("/transaction").route(web::post().to(handle_post))
+pub fn init_service() -> Scope {
+    web::scope("/transaction")
+        .service(web::resource("").route(web::post().to(handle_post)))
+        .service(web::resource("/broadcast").route(web::post().to(handle_broadcast)))
+}
+
+async fn handle_broadcast(
+    transaction: web::Json<Transaction>,
+    app_state: web::Data<AppState>,
+) -> Result<HttpResponse, error::Error> {
+    let transaction = transaction.0;
+
+    match app_state.blockchain.lock() {
+        Ok(mut blockchain) => {
+            let o = &blockchain.create_new_transaction(transaction.clone());
+            let client = Client::new();
+
+            futures::future::join_all(blockchain.network_nodes.iter().map(|node| {
+                let client = &client;
+                let transaction = &transaction;
+                async move {
+                    let url = format!("http://localhost:{}/transaction", node.address);
+                    client.post(&url).json(&transaction).send().await
+                }
+            }))
+            .await;
+
+            let res = json!({ "note": format!("it will be part of Block {}", o) });
+            Ok(HttpResponse::Ok().json(res))
+        }
+        Err(e) => Err(error::ErrorInternalServerError(e.to_string())),
+    }
 }
 
 async fn handle_post(
-    mut payload: web::Payload,
+    transaction: web::Json<Transaction>,
     app_state: web::Data<AppState>,
 ) -> Result<HttpResponse, error::Error> {
-    let mut body = BytesMut::new();
-    while let Some(chunk) = payload.next().await {
-        let chunk = chunk?;
-        body.extend_from_slice(&chunk);
-    }
+    let transaction = transaction.0;
 
-    match serde_json::from_slice::<Transaction>(&body) {
-        Ok(res) => match app_state.blockchain.lock() {
-            Ok(mut blockchain) => {
-                let o = &blockchain.create_new_transaction(res);
-                let res = json!({ "note": format!("it will be part of Block {}", o) });
-                Ok(HttpResponse::Ok().json(res))
-            }
-            Err(e) => Err(error::ErrorInternalServerError(e.to_string())),
-        },
-        Err(e) => Err(error::ErrorBadRequest(e)),
+    match app_state.blockchain.lock() {
+        Ok(mut blockchain) => {
+            let o = &blockchain.create_new_transaction(transaction);
+            let res = json!({ "note": format!("it will be part of Block {}", o) });
+            Ok(HttpResponse::Ok().json(res))
+        }
+        Err(e) => Err(error::ErrorInternalServerError(e.to_string())),
     }
 }
